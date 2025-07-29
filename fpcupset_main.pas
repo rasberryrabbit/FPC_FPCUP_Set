365a72b9651fe21ceab4ec599d3a8ac9df06460c
rasberryrabbit <purplecofe2@gmail.com>
2025-07-29 21:40:01 +0900

    Fix replacing package files in XML.

--- fpcupset_main.pas
+++ fpcupset_main.pas
@@ -13,24 +13,22 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
-    Edit4: TEdit;
+    EdLaz: TEdit;
+    EdFPC: TEdit;
+    EdFPCDIR: TEdit;
+    FPC: TLabel;
+    FPCDIR: TLabel;
+    Lazarus: TLabel;
     Memo1: TMemo;
     procedure Button1Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
     function Patch_fpc_cfg(const File_Path: string; new_path,
       fpc_dirname: string): string;
-    function Patch_lazarus_cfg(const File_Path: string; new_path: string;
-      customdir: array of string): string;
-    procedure Patch_lazarus_xml(const File_Path: string; new_path: string;
-      customdir: array of string);
-    function str_lazarus_cfg(const Source: string; new_path: string;
-      customdir: array of string): string;
-    function str_lazarus_xml(const Source: string; new_path: string;
-      customdir: array of string): string;
+    function Patch_lazarus_cfg(const File_Path: string; new_path: string): string;
+    procedure Patch_lazarus_xml(const File_Path: string; new_path: string);
+    function str_lazarus_cfg(const Source: string; new_path: string): string;
+    function str_lazarus_xml(const Source: string; new_path: string): string;
     { private declarations }
   public
     { public declarations }
@@ -45,84 +43,62 @@ uses shlobj, Laz2_DOM, laz2_XMLRead, laz2_XMLWrite, RegExpr, strutils;
 
 {$R *.lfm}
 
-const
-  config_folder='config_lazarus';
-  config_fpc='fpc\bin\i386-win32\fpc.cfg';
-
 var
   newpath:string;
 
 
 const
+  config_folder='config_lazarus';
+  config_fpc='fpc\bin\i386-win32\fpc.cfg';
+
   lazarus_cfg = 'idemake.cfg';
-  lazarus_xml : array[0..5] of string = (
+  lazarus_xml : array[0..8] of string = (
                 'codetoolsoptions.xml',
                 'editoroptions.xml',
                 'environmentoptions.xml',
                 'fpcdefines.xml',
                 'inputhistory.xml',
-                'packagefiles.xml'
+                'packagefiles.xml',
+                'debuggeroptions.xml',
+                'helpoptions.xml',
+                'pas2jsdsgnoptions.xml'
                 );
-type
-  TArrayString = array of string;
-
-  procedure StrToArray(const s:string; var sarray:TArrayString);
-  var
-    i, start, len : Integer;
-  begin
-    SetLength(sarray,0);
-    start := 1;
-    len := Length(s);
-    if len>0 then begin
-      repeat
-        i:= PosEx(';',s,Start);
-        if i=0 then
-          i := len+1;
-        SetLength(sarray,Length(sarray)+1);
-        sarray[Length(sarray)-1]:=Copy(s,Start,i-Start);
-        Start:=i+1;
-      until i>=len;
-    end;
-  end;
 
 { TForm1 }
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
   newpath:=ExtractFilePath(ParamStr(0));
-  Edit1.Text:=newpath+config_folder;
-  Edit2.Text:=newpath+config_fpc;
+  EdLaz.Text:=newpath+config_folder;
+  EdFPC.Text:=newpath+config_fpc;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
   xml_path, lazcfg_path, fpccfg_path : string;
   i : Integer;
-  sarray : array of string;
 begin
   Memo1.Clear;
-  SetLength(sarray,0);
-  StrToArray(Edit4.Text,sarray);
   Memo1.Lines.Add('New Path : '+newpath);
   // patch fpc cfg file
-  fpccfg_path:=Edit2.Text;
+  fpccfg_path:=EdFPC.Text;
   if FileExists(fpccfg_path) then begin
-    Patch_fpc_cfg(fpccfg_path,newpath,Edit3.Text);
+    Patch_fpc_cfg(fpccfg_path,newpath,EdFPCDIR.Text);
     Memo1.Lines.Add('Patched : '+ fpccfg_path);
   end else
     Memo1.Lines.Add('File not found : '+fpccfg_path);
   // patch cfg files
-  lazcfg_path:=Edit1.Text+PathDelim+lazarus_cfg;
+  lazcfg_path:=EdLaz.Text+PathDelim+lazarus_cfg;
   if FileExists(lazcfg_path) then begin
-    Patch_lazarus_cfg(lazcfg_path,newpath,sarray);
+    Patch_lazarus_cfg(lazcfg_path,newpath);
     Memo1.Lines.Add('Patched : '+lazarus_cfg);
   end else
     Memo1.Lines.Add('File not found : '+lazcfg_path);
   // patch xml files
   for i:=Low(lazarus_xml) to High(lazarus_xml) do begin
-    xml_path:=Edit1.Text+PathDelim+lazarus_xml[i];
+    xml_path:=EdLaz.Text+PathDelim+lazarus_xml[i];
     if FileExists(xml_path) then begin
-        Patch_lazarus_xml(Edit1.Text+PathDelim+lazarus_xml[i],newpath,sarray);
+        Patch_lazarus_xml(EdLaz.Text+PathDelim+lazarus_xml[i],newpath);
         Memo1.Lines.Add('Patched : '+lazarus_xml[i]);
       end
       else
@@ -130,6 +106,25 @@ begin
   end;
 end;
 
+function RemovePreDir(const path: string):string;
+var
+  i, j, k: Integer;
+begin
+  Result:='';
+  i:=1;
+  j:=Length(path);
+  k:=0;
+  while i<=j do begin
+    if path[i] in ['\','/'] then begin
+      Inc(k);
+      if k=2 then begin
+        Result:=Copy(path,i);
+        break;
+      end;
+    end;
+    inc(i);
+  end;
+end;
 
 function TForm1.Patch_fpc_cfg(const File_Path: string; new_path,
   fpc_dirname: string): string;
@@ -155,7 +150,7 @@ begin
     end;
     Inc(i);
   end;
-  if FileExistsUTF8(File_path) then begin
+  if FileExists(File_path) then begin
     InBuff := TStringList.Create;
     try
       Inbuff.LoadFromFile(File_path);
@@ -182,17 +177,17 @@ begin
   end;
 end;
 
-function TForm1.Patch_lazarus_cfg(const File_Path: string; new_path: string;
-  customdir : array of string): string;
+function TForm1.Patch_lazarus_cfg(const File_Path: string; new_path: string
+  ): string;
 var
   Inbuff: TStringList;
 begin
   Result:='';
-  if FileExistsUTF8(File_path) then begin
+  if FileExists(File_path) then begin
     InBuff := TStringList.Create;
     try
       Inbuff.LoadFromFile(File_path);
-      Result := str_lazarus_cfg(Inbuff.Text,new_path,customdir);
+      Result := str_lazarus_cfg(Inbuff.Text,new_path);
       Inbuff.Text:=Result;
       Inbuff.SaveToFile(File_Path);
     finally
@@ -201,57 +196,51 @@ begin
   end;
 end;
 
-function TForm1.str_lazarus_cfg(const Source: string; new_path: string;
-  customdir: array of string): string;
+function TForm1.str_lazarus_cfg(const Source: string; new_path: string): string;
 const
-  lazarus_pattern1 = '([a-z,A-Z]\:)?[\\/][^\n]+([\\/]((?i)';
-  lazarus_pattern2 = ')[\\/]?[^\n]+)';
+  lazarus_pattern = '(\-F.)([^\n;]+)';
 var
-  len: Integer;
-  i: Integer;
-  resstr: string;
+  i, len, sp, ep: Integer;
+  old_path: string;
   reg_fpccfg: RegExpr.TRegExpr;
-  regstr : string;
 begin
   Result:='';
   i := 1;
   new_path:=ExtractFileDir(new_path);
   len := length(new_path);
-  while i<=len do begin
-    if new_path[i] in ['\', '.', '[', ']', '(', ')', '$', '^', '-', '+']
-      then begin
-      Insert('\', new_path, i);
-      Inc(i); Inc(len);
-    end;
-    Inc(i);
-  end;
-  regstr := lazarus_pattern1;
-  for i:=0 to Length(customdir)-1 do
-    if customdir[i]<>'' then
-      if i>0 then
-        regstr:=regstr+'|'+customdir[i]
-        else
-          regstr:=regstr+customdir[i];
-  regstr:=regstr+lazarus_pattern2;
-  reg_fpccfg := TRegExpr.Create(regstr);
+
+  reg_fpccfg := TRegExpr.Create(lazarus_pattern);
   try
     reg_fpccfg.ModifierM:=True;
+    sp:=1;
+    ep:=1;
     if reg_fpccfg.Exec(Source) then begin
-      (*
       repeat
-        Memo1.Lines.Add('=======================');
-        Memo1.Lines.Add(reg_fpccfg.Match[0]+'!'+reg_fpccfg.Match[2]);
+        ep:=reg_fpccfg.MatchPos[0];
+        Result:=Result+Copy(Source,sp,ep-sp);
+        sp:=ep;
+
+        old_path:=RemovePreDir(reg_fpccfg.Match[2]);
+        while old_path<>'' do begin
+          if DirectoryExists(reg_fpccfg.Match[1]+new_path+old_path) then begin
+            Result:=Result+reg_fpccfg.Match[1]+new_path+old_path;
+            break;
+          end;
+          old_path:=RemovePreDir(old_path);
+        end;
+        if old_path<>'' then
+          sp:=ep+reg_fpccfg.MatchLen[0];
       until not reg_fpccfg.ExecNext;
-      *)
-      Result:=reg_fpccfg.Replace(Source, new_path+'$2', True);
+
+      if len >= sp then
+        Result:=Result+Copy(Source,sp,len-sp);
     end;
   finally
     reg_fpccfg.Free;
   end;
 end;
 
-procedure TForm1.Patch_lazarus_xml(const File_Path: string; new_path: string;
-  customdir: array of string);
+procedure TForm1.Patch_lazarus_xml(const File_Path: string; new_path: string);
 var
   Child : TDOMNode;
   Doc : TXMLDocument;
@@ -269,7 +258,7 @@ var
     if Node.HasAttributes and (Node.Attributes.Length>0) then
       for i:=0 to Node.Attributes.Length-1 do begin
         s:=Node.Attributes[i].NodeValue;
-        s := str_lazarus_xml(s,new_path,customdir);
+        s := str_lazarus_xml(s,new_path);
         if s<>'' then begin
           Node.Attributes[i].NodeValue:=s;
         end;
@@ -299,39 +288,39 @@ begin
   end;
 end;
 
-function TForm1.str_lazarus_xml(const Source: string; new_path: string;
-  customdir: array of string): string;
+function TForm1.str_lazarus_xml(const Source: string; new_path: string): string;
 const
-  lazarus_pattern1 = '((?i)[\\/]';
-  lazarus_pattern2 = '[\\/]?)';
+  FilePath_Pattern = '[\\/][^\.\\/]+\.[^\.\\/]+';
 var
   len: Integer;
   i: Integer;
   resstr: string;
   reg_fpccfg: RegExpr.TRegExpr;
-  regstr : string;
+  old_path: string;
 begin
   Result:='';
-  i := 1;
+  i:= 1;
   new_path:=ExtractFileDir(new_path);
-  for i:=0 to Length(customdir)-1 do
-    if customdir[i]<>'' then begin
-      regstr := lazarus_pattern1+customdir[i]+lazarus_pattern2;
-      reg_fpccfg:=TRegExpr.Create(regstr);
-      try
-        reg_fpccfg.ModifierM:=True;
-        if reg_fpccfg.Exec(Source) then begin
-          Result:=new_path+ Copy(Source,reg_fpccfg.MatchPos[1],Length(Source)-reg_fpccfg.MatchPos[1]+1);
-          (*
-          Memo1.Lines.Add('==========================================');
-          Memo1.Lines.Add(Result);
-          *)
+  reg_fpccfg:=TRegExpr.Create(FilePath_Pattern);
+  try
+    reg_fpccfg.ModifierM:=True;
+    if reg_fpccfg.Exec(Source) then begin
+      old_path:=Source;
+      repeat
+        old_path:=RemovePreDir(old_path);
+        if (old_path<>'') and
+           ( FileExists(new_path+old_path) or
+             DirectoryExists(new_path+old_path) )
+        then begin
+          Result:=new_path+old_path;
+          //Memo1.Lines.Add(Result);
           break;
         end;
-      finally
-        reg_fpccfg.Free;
-      end;
+      until old_path='';
     end;
+  finally
+    reg_fpccfg.Free;
+  end;
 end;
 
 
